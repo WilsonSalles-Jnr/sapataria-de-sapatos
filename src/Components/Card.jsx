688d407504aaae4e3ffb02837ca4c00262287936
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2022-05-21 11:13:22 -0300

    feat: base Card feito

---
+++ src/Components/Card.jsx
@@ -0,0 +1,11 @@
+export default function Card() {
+  return(
+    <main style={{display: 'flex'}}>
+      <p className="cards">este é um card</p>
+      <p className="cards">este é um card</p>
+      <p className="cards">este é um card</p>
+      <p className="cards">este é um card</p>
+      <p className="cards">este é um card</p>
+    </main>
+  )
+}
