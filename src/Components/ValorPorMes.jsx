953021d6a96ae1962313bace71bf998c56a423a0
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2022-05-25 23:01:37 -0300

    feat: feito grafico de valor por mes

---
+++ src/Components/ValorPorMes.jsx
@@ -0,0 +1,35 @@
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from 'recharts'
+
+export default function ValorPorMes({data}) {
+  return(
+    <article className='resultBox'>
+      <h1>Valor Arrecadado Por Mês</h1>
+      <LineChart
+        width={650}
+        height={150}
+        data={data}
+        margin={{top: 5, right: 30, left: 20, bottom: 5}}
+        >
+        <Line
+          type='monotone'
+          dataKey='reais'
+          stroke='#8884d8'
+          activeDot={{r: 8}}
+          />
+        <CartesianGrid strokeDasharray='3 3'/>
+        <Tooltip/>
+        <YAxis/>
+        <XAxis dataKey='mes'/>
+        <Legend />
+      </LineChart>
+    </article>
+  )
+}
